052f27e2063895e394943f57a22b2ac3996a6da7
amansarb <aman.lotey1986@gmail.com>
2020-07-07 13:44:22 +1200

    Testing of google search bar

--- myFirstTestCase/myFirstTestCase/UnitTest1.cs
+++ myFirstTestCase/myFirstTestCase/UnitTest1.cs
@@ -3,6 +3,7 @@ using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
+using OpenQA.Selenium.Internal;
 
 namespace myFirstTestCase
 {
@@ -12,8 +13,12 @@ namespace myFirstTestCase
         public void Test1()
         {
             IWebDriver driver = new ChromeDriver();
-            // driver.Url = "http://google.com";
-            driver.Url = "http://techexpozed.co.nz";
+            driver.Url = "http://google.com";
+            //driver.Url = "http://techexpozed.co.nz";
+            IWebElement web = driver.FindElement(By.Name("q"));
+            web.SendKeys("God is one, God helps everyone");
+            web.Submit();
+            Console.WriteLine(driver.Title);
             Thread.Sleep(2000);
             driver.Quit();
         }

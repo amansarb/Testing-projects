15893b7b6357ca00293c176cd3d3e3874eaea238
amansarb <aman.lotey1986@gmail.com>
2020-07-28 13:16:29 +1200

    Tech Exposed Contact form tested as a task

---
+++ TechExpozedContactForm/TechExpozedContactForm/UnitTest1.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+
+namespace TechExpozedContactForm
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TechExpoContactForm()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url=("https://techexpozed.co.nz/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Contact")).Click();                         // Contact page opened
+            driver.FindElement(By.Id("name")).SendKeys("Aman");                             // Name
+            driver.FindElement(By.Name("email")).SendKeys("aman.lotey1986@gmail.com");      //email
+            IWebElement element = driver.FindElement(By.Name("subject"));                   // choose topic
+            SelectElement select = new SelectElement(element);
+            select.SelectByIndex(4);
+            driver.FindElement(By.Id("message")).SendKeys("Hi one of the intern, \n doing automatic testing");      // type message
+            driver.FindElement(By.XPath("//*[@id='contact-form']/div/div[5]/button")).Click();                      // click send message
+             Thread.Sleep(2000);
+            driver.Quit();
+
+        }
+        [TestMethod]
+        public void ValidityCheckContactFormNoName()              //Leave Name field empty
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = ("https://techexpozed.co.nz/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Contact")).Click();                         // Contact page opened
+            driver.FindElement(By.Id("name")).SendKeys("");                             // Name
+            driver.FindElement(By.Name("email")).SendKeys("aman.lotey1986@gmail.com");      //email
+            IWebElement element = driver.FindElement(By.Name("subject"));                   // choose topic
+            SelectElement select = new SelectElement(element);
+            select.SelectByIndex(4);
+            driver.FindElement(By.Id("message")).SendKeys("Hi one of the intern, \n doing automatic testing");      // type message
+            driver.FindElement(By.XPath("//*[@id='contact-form']/div/div[5]/button")).Click();                      // click send message
+            Thread.Sleep(2000);
+            driver.Quit();
+
+        }
+        [TestMethod]
+        public void NoEmail()              //Validy check with no email entered 
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = ("https://techexpozed.co.nz/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Contact")).Click();                         // Contact page opened
+            driver.FindElement(By.Id("name")).SendKeys("Aman");                             // Name
+            driver.FindElement(By.Name("email")).SendKeys("");      //email
+            IWebElement element = driver.FindElement(By.Name("subject"));                   // choose topic
+            SelectElement select = new SelectElement(element);
+            select.SelectByIndex(4);
+            driver.FindElement(By.Id("message")).SendKeys("Hi one of the intern, \n doing automatic testing");      // type message
+            driver.FindElement(By.XPath("//*[@id='contact-form']/div/div[5]/button")).Click();                      // click send message
+            Thread.Sleep(2000);
+            driver.Quit();
+
+        }
+        [TestMethod]
+        public void Nomessage()                 //Validy check with no message
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = ("https://techexpozed.co.nz/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Contact")).Click();                         // Contact page opened
+            driver.FindElement(By.Id("name")).SendKeys("Aman");                             // Name
+            driver.FindElement(By.Name("email")).SendKeys("aman.lotey1986@gmail.com");      //email
+            IWebElement element = driver.FindElement(By.Name("subject"));                   // choose topic
+            SelectElement select = new SelectElement(element);
+            select.SelectByIndex(4);
+            driver.FindElement(By.Id("message")).SendKeys("");      // type message
+            driver.FindElement(By.XPath("//*[@id='contact-form']/div/div[5]/button")).Click();                      // click send message
+            Thread.Sleep(2000);
+            driver.Quit();
+
+        }
+    }
+}
